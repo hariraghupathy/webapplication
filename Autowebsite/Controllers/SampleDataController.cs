babbc64f90dc0ae3f50edb1f779bb4d116fe68aa
harikrishnan <hariraghupathy@gmail.com>
2019-03-01 01:42:01 +0530

    Second Commit

--- Autowebsite/Controllers/SampleDataController.cs
+++ Autowebsite/Controllers/SampleDataController.cs
@@ -11,7 +11,7 @@ namespace Autowebsite.Controllers
     {
         private static string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Raining", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         [HttpGet("[action]")]
